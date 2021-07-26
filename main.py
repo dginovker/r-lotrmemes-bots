f453a7908a2bfcb88a4a402f98acd3b53191940e
dginovker <dcress01@uoguelph.ca>
2021-07-26 17:50:56 -0400

    Remove debug

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ trigger_dictionary = {
     ("hobbits", ): ["Stupid, fat hobbit!"],
     ("Po-ta-toes", "taters", ): ["Whats taters, precious? What's taters, eh?"],
     ("baggins", "bagginses", ): ["SHIIIIIIRE! BAGGINS!"],
+    ("pocketses", ): ["What has it got in its pocketses?"],
 }
 
 ignore_authors = {"smeagol-bot", }
@@ -51,12 +52,8 @@ def handle_comment(comment):
         return  # Don't reply to any comment made by someone in ignore_authors
 
     reply = get_comment_reply(comment.body)
-    if reply is not None:
-        print(
-            comment.author.name + " triggered with [" + comment.body + "]. Responding with [" + reply + "]!")
+    if reply is not None:  # If we found a reply for the comment
         comment.reply(reply)
-
-    print(comment.body + " -- Had no triggers")
 
 
 def scan_comments():
